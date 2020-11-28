a9d84f183e583c1400ec27e07f1bd1a9c0ba4a40
Leonardo Alvarado <10leoalvarado@gmail.com>
2020-11-28 10:27:11 -0600

    Footer Probando Aun

--- fntsagas/src/cmps/cmns/Footer.js
+++ fntsagas/src/cmps/cmns/Footer.js
@@ -1,7 +1,16 @@
 import {MdHome} from 'react-icons/md';
-import {FaUserPlus} from 'react-icons/fa';
+import {FaFacebook, FaInstagram, FaTwitch, FaTwitter, FaUserPlus, FaYoutube} from 'react-icons/fa';
 import { IoMdHand } from 'react-icons/io';
-import  {NavLink} from 'react-router-dom';
+import  {Link, NavLink} from 'react-router-dom';
+//import SocialMediaIcons from 'react-social-media-icons';
+import { FcGoogle } from 'react-icons/fc';
+
+
+
+// import React from 'react';
+// import  ReactDOM  from  'react-dom';
+// import {SocialMediaIconsReact} from 'social-media-icons-react';
+// ReactDOM.render(<SocialMediaIconsReact icon="twitter" url="https://twitter.com/your-twitter-handle"/>,	document.getElementById('root'));
 
 import "./Footer.css";
 const Footer = ()=>{
@@ -14,7 +23,20 @@ const Footer = ()=>{
         <li><NavLink to="/signin"><IoMdHand size="1.5em"></IoMdHand></NavLink></li>
         <li><NavLink to="/productos"><IoMdHand size="1.5em"></IoMdHand></NavLink></li>
         </ul>
+        <div class="footer-main-div">
+            <div class="footer-social-icons">
+                <ul>
+                    {/* <li><FcGoogle size="35"/></li> */}
+                    <li><NavLink to="/"><FaFacebook size="1.5em"/></NavLink></li>
+                    <li><NavLink to="/"><FaInstagram size="1.5em"/></NavLink></li>
+                    <li><NavLink to="/"><FaTwitter size="1.5em"/></NavLink></li>
+                    <li><NavLink to="/"><FaYoutube size="1.5em"/></NavLink></li>
+                </ul>
+            </div>
+        </div>
       </nav>
+      
+      
     </footer>
   )
 }
