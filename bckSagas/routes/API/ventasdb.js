919e005502ff13e7fab41d02089a80be643a9ca7
Hesler Ferrera <heslerferrera@gmail.com>
2020-11-27 23:54:16 -0600

    Update ventasdb.js
    Agregando la ruta para agregar una nueva venta

--- bckSagas/routes/API/ventasdb.js
+++ bckSagas/routes/API/ventasdb.js
@@ -1,17 +1,31 @@
 const express = require("express");
-const VentasModel = require("../../models/ventas/ventas.model");
 let router = express.Router();
 
-let ventasModel = require('../../models/ventas/ventas.model')();
+let ventasModel = require('../../models/ventas/ventas.model');
 const mdbVentasModel = new ventasModel();
 
 
-router.get('/ventas/:tm1', async (req, res)=>{
+router.get('/ventas/:date', async (req, res)=>{
     try {
-        let { tm1 } = req.params;
-        let rsltset = await mdbVentasModel.
+        let { date } = req.params;
+        let rsltset = await mdbVentasModel.getByDate({Date:date});
+        res.status(200).json(rsltset);
     } catch (ex) {
         console.log(ex);
         res.status(500).json({ "msg": "Algo Paso Mal." });
     }
 }); 
+
+router.post('new', async (res, req)=>{
+    try {
+        let {date, name, category ,price, stock=0} = req.body;
+        price = Number(price);
+        stock = Number(stock);
+        var rsltset = await mdbVentasModel.addVenta({date, name, category, price, stock});
+    } catch (ex) {
+        console.log(ex);
+        res.status(500).json({"msg":"Algo Paso Mal."});
+    }
+}); 
+
+module.exports = router;
