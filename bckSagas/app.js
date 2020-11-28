9cddb697a8c5b21dfebff80ce80c372295016535
Fernando <flopezbarahona1998@gmail.com>
2020-11-27 22:40:18 -0600

    Update app.js

--- bckSagas/app.js
+++ bckSagas/app.js
@@ -7,12 +7,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-<<<<<<< HEAD
 var apiRouter = require('./routes/api');
 
-=======
-var apiRouter = require('./routes/API');
->>>>>>> ed7a0ebbeea8dbd323afd17444b2ee8aaaf6498d
 
 var app = express();
 
@@ -29,11 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-<<<<<<< HEAD
 app.use('/api', apiRouter);
-=======
-app.use('/API', apiRouter);
->>>>>>> ed7a0ebbeea8dbd323afd17444b2ee8aaaf6498d
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
