802caa38677d4e6729b36d75822c7f601cdce8be
Fernando <flopezbarahona1998@gmail.com>
2020-11-28 02:10:23 -0600

    Revert "Seguridad"
    This reverts commit beb3847b7e6b31ba15e8e45b5d87eeb2275b4181.

--- bckSagas/routes/api.js
+++ bckSagas/routes/api.js
@@ -1,34 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-let passport = require('passport');
-let passportJWT = require('passport-jwt');
-
-let extractJWT = passportJWT.ExtractJwt;
-let strategyJWT = passportJWT.Strategy;
-
-passport.use(
-  new strategyJWT(
-    {
-      jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET
-    },
-    (payload, next)=>{
-       var user = payload;
-       return next(null, user);
-    }
-  )
-)
-
-const jwtAuthMiddleware = passport.authenticate('jwt', {session:false});
-
 const ventasRoutes = require('./api/ventasdb');
 const productosRoutes = require('./api/productosdb');
-const seguridadRoutes = require('./api/seguridad');
 
 
 router.use('/ventas', ventasRoutes)
 router.use('/productos', productosRoutes)
-router.use('/seguridad', seguridadRoutes)
 
 module.exports = router;
