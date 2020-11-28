ef29f7b3857ebba5764e28d7e7d21c3f615aa78a
Fernando <flopezbarahona1998@gmail.com>
2020-11-28 00:20:47 -0600

    carpeta productos

--- bckSagas/routes/api.js
+++ bckSagas/routes/api.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const ventasRoutes = require('./api/ventasdb');
+
+router.use('/ventas', ventasRoutes)
 
 module.exports = router;
