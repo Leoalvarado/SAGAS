809a51a22cda5542e49f71528792d324b0bf7840
Leonardo Alvarado <10leoalvarado@gmail.com>
2020-11-29 23:40:05 -0600

    Menu

--- fntsagas/src/cmps/cmns/Footer.js
+++ fntsagas/src/cmps/cmns/Footer.js
@@ -22,7 +22,7 @@ const Footer = ()=>{
         <ul>
             <li><NavLink to="/"><Home size="1.5em"/></NavLink></li>
             <li><NavLink to="/login"><Category size="1.5em"></Category></NavLink></li> 
-            <li><NavLink to="/productos"><AddShoppingCart icon="dashboard" size="1.5em" /></NavLink></li>
+            <li><NavLink to="/menu"><AddShoppingCart icon="dashboard" size="1.5em" /></NavLink></li>
             <li><NavLink to="/productos"><EmojiPeople icon="dashboard" size="1.5em" /></NavLink></li>
         </ul>
       </nav>
