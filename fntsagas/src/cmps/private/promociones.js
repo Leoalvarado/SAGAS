542a329ebaa9e436570b8acb55c66dc0c50e6853
Willard19 <60764938+Willard19@users.noreply.github.com>
2020-12-10 04:27:33 -0600

    Agregando pagina de promocion.
    Falta mas diseño.

--- fntsagas/src/cmps/private/promociones.js
+++ fntsagas/src/cmps/private/promociones.js
@@ -3,31 +3,88 @@ import {Redirect} from 'react-router-dom';
 import logo from "../public/img/SagasCreationLogo.png";
 import Page from '../cmns/Page';
 import "./promociones.css";
-import img1 from "../../public/img/promo1";
-
-
-
+import img1 from "../public/img/promo1.png";
+import img2 from "../public/img/promo2.png";
+import img3 from "../public/img/promo3.png";
+import img4 from "../public/img/promo4.png";
+import img5 from "../public/img/promo5.png";
+import img6 from "../public/img/promo6.png";
+import img7 from "../public/img/promo7.png";
+import img8 from "../public/img/promo8.png";
+import img9 from "../public/img/promo9.png";
+import img10 from "../public/img/promo10.png";
 
 
 const Promociones = ()=>{
 
-     /*
-     const listElements = dummydata.map((o) =>{
-    return (<li key={o._id}>{o.label}<span>{o.count}</span></li>)
-    })
-     */   
      let[redirect,setRedirect]=useState("");
     if(redirect!==""){
         return(<Redirect to={redirect}></Redirect>);
     }
     
     return(
-        <Page heading="_" footer={true}>
+        <Page heading="Promociones" footer={true}>
            <section className="promsection">
-                <img src={logo} className="logoex"/>
+                
                 <div className="galeria">
                     <img src={img1}/>
-                </div>            
+                    <button>IR AQUÍ</button>
+                </div>          
+
+                <div className="galeria">
+                    <img src={img2}/>
+                    <button>IR AQUÍ</button>
+                </div>
+
+                <div className="galeria">
+                    <img src={img3}/>
+                    <button>IR AQUÍ</button>
+                </div> 
+
+                <div className="galeria">
+                    <img src={img4}/>
+                    <button>IR AQUÍ</button>
+                </div>   
+
+                <div className="galeria">
+                    <img src={img5}/>
+                    <button>IR AQUÍ</button>
+                </div>   
+
+                <div className="galeria">
+                    <img src={img6}/>
+                    <button>IR AQUÍ</button>
+                </div>   
+
+                <div className="galeria">
+                    <img src={img7}/>
+                    <button>IR AQUÍ</button>
+                </div>   
+
+                <div className="galeria">
+                    <img src={img8}/>
+                    <button>IR AQUÍ</button>
+                </div>   
+
+                <div className="galeria">
+                    <img src={img10}/>
+                    <button>IR AQUÍ</button>
+                </div>   
+
+                <div className="galeria">
+                    <img src={img9}/>
+                    <button>IR AQUÍ</button>
+                </div>   
+
+                <div className="galeria">
+                    <img src={img7}/>
+                    <button>IR AQUÍ</button>
+                </div>   
+
+                <div className="galeria">
+                    <img src={img6}/>
+                    <button>IR AQUÍ</button>
+                </div>                   
            </section>
 
         </Page>
