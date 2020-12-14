332646e20259848a10b1d9619cc64b811a8774bf
Leonardo Alvarado <10leoalvarado@gmail.com>
2020-12-14 01:18:38 -0600

    Heroku (Almost Ready)

--- bckSagas/app.js
+++ bckSagas/app.js
@@ -27,6 +27,10 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
+//Conf Atlas y Heroku
+app.get('/*', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
