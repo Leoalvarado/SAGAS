d4a10122e45edb37cd41aa527fcd72028fff1ca5
Leonardo Alvarado <10leoalvarado@gmail.com>
2020-12-14 23:05:51 -0600

    Final finalito

--- fntsagas/src/cmps/private/cumple.js
+++ fntsagas/src/cmps/private/cumple.js
@@ -6,38 +6,56 @@ import { useHistory } from 'react-router-dom';
 import { AddButton } from '../cmns/Buttons';
 import logo from "../public/img/SagasCreationLogo.png";
 import Page from '../cmns/Page';
-import {paxios} from '../../utlts/Axios';  
+import {naxios, paxios} from '../../utlts/Axios'; 
 import "./cumple.css";
 import {useStateContext } from '../../utlts/Context';
 import { PRODUCT_ERROR, PRODUCT_LOADED, PRODUCT_LOADING } from '../../utlts/store/reducers/prods.reducer';
+import promo1 from "../public/img/promo1.png";
 
 
 const Cumple = ()=>{
 
     const [{prods}, dispatch] = useStateContext();
-    const history = useHistory();
+    function addToCart( sku, name, price){
+        naxios.post('/api/carretilla/new', {"sku":sku,"name": name,"price": price})
+        .then(({data})=>{
+            //dispatch({type:PRODUCT_LOADED, payload:data});
+            console.log(data);
+        })
+        .catch((ex)=>{
+            //dispatch({ type: PRODUCT_ERROR});
+            console.log(ex)
+        });    
+    //window.location.reload(); 
+    }
     const listElements = prods.products.map((o) =>{
-    return (
-        <li key={o._id}>
-            <div className="title">
-                {o.sku}
-            </div>
-            <b className="nombre"> 
-                {o.name}
-            </b>
-            <b className="precio">
-                {o.precio}
-            </b>
-            
-            <a className="buttom" href="#"><b>Add to Cart</b></a>
-        </li>)
-    })
+        return (
+            <li key={o._id}>
+                <div class="img-container">
+                    <img src={promo1}/>
+                    <span class="promo">15% de descuento</span>
+                </div>
+                <div>
+                    {o.sku}
+                </div>
+                <b> 
+                    {o.name}
+                </b>
+                <b>
+                    ${o.precio}
+                </b>
+                <span class="rating">★★★★☆</span>
+                <a href="#" class="add-cart" onClick={()=>addToCart( o.sku, o.name, o.price)}>Añadir al carrito</a>
+                {/* <a className="buttom" onClick={()=>addToCart( o.sku, o.name, o.price)}><b>Add to Cart</b></a> */}
+                
+            </li>)
+        })
 
     
     useEffect(
         ()=>{
             dispatch({ type: PRODUCT_LOADING})
-            paxios.get('api/productos/productoCategoria/Cumpleaños')  // ese ultimo pleca nos jala exclusivamente solo para cumple y es lo que debemos hacer!
+            paxios.get('/api/productos/productosAll')  // ese ultimo pleca nos jala exclusivamente solo para cumple y es lo que debemos hacer!
             .then(({data})=>{
                 dispatch({type:PRODUCT_LOADED, payload:data});
                 console.log(data);
@@ -56,12 +74,18 @@ const Cumple = ()=>{
     }
     return(
         <Page heading="Cumpleaños" footer={true}>
-           <section className="listasection"> 
-                <ul className="menuList">
-                    {listElements}
-                </ul>
-                
-           </section>
+           <section className="listSec">
+                <div class="card estilo-c">
+                    <div class="info-container">
+                        <h3>
+                            <ul className="menuLi">
+                                {listElements}
+                            </ul>
+                        </h3>
+                        
+                    </div>
+                </div>
+            </section>
         </Page>
     )
 }
